9e5a0f1c6b95b6d663d72399c8d2593b4088a541
Eberechi <eberewill@gmail.com>
2022-07-10 03:09:45 +0100

    updates

--- src/api/cosmos.js
+++ src/api/cosmos.js
@@ -2,7 +2,40 @@ const express = require('express');
 const puppeteer = require("puppeteer-extra");
 const router = express.Router();
 
+
 router.get('/', async (req, res) => {
+  try {
+    const url = 'https://7nkwv3z5t1.execute-api.us-east-1.amazonaws.com/prod/listData?type=getPool&address=cosmosvaloper17mggn4znyeyg25wd7498qxl7r2jhgue8u4qjcq&frequencyHour=8H&frequencyDay=10D&key=2mwTEDr9zXJH323M&token=1657413873&app=ATOM'
+ await puppeteer
+      .launch({
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+        ],
+      })
+      .then(async (browser) => {
+        const page = await browser.newPage();
+        await page.setViewport({
+          width: 1366,
+          height: 768,
+        });
+        page.on("response", async (response) => {
+           const mdata = await response.json();
+           res.json(mdata);
+        });
+        await page.goto(url, {
+          waitUntil: "load",
+          timeout: 0,
+        });
+      });
+    
+  } catch (error) {
+    res.json({ error: "server error", data: error.message });
+
+  }
+});
+
+router.get('/stats', async (req, res) => {
   try {
     const url = 'https://7nkwv3z5t1.execute-api.us-east-1.amazonaws.com/prod/listData?type=valstats&frequencyNetwork=All&address=cosmosvaloper17mggn4znyeyg25wd7498qxl7r2jhgue8u4qjcq&key=2mwTEDr9zXJH323M&token=1657402061&app=ATOM'
     await puppeteer
